eba74fcbc00775aceb8a4a4fd1879ce4d02bd03c
tkrhinton <thomas.hinton@students.plymouth.ac.uk>
2019-12-29 23:16:32 +0000

    Develop Header and footer files for the application

---
+++ public/home.php
@@ -0,0 +1,49 @@
+<?php
+include_once 'header.php';
+?>
+
+
+<head>
+    <title> Home Page </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/structure.css">
+</head>
+
+<body>
+<style>
+    body  {
+        background-image: url("Background.jpg");
+        background-repeat:no-repeat;
+        background-size: 2500px 1000px;
+    }
+</style>
+
+
+
+<div class="container-fluid" style="margin-top:30px" >
+    <div class="row">
+        <div class="col-sm-8">
+            <div style="background-color:hsla(200,60%,70%,0.5);">
+                <h1>  Application </h1>
+
+                <p> This the the home page for the  application</p>
+
+
+            </div>
+
+
+        </div>
+
+        <div class="col-sm-4">
+            <h1>hi</h1>
+        </div>
+
+
+
+
+
+<?php
+include_once 'footer.php';
+?>
