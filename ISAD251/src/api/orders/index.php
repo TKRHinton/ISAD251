57928138645e3a5dcf58f24a99804838c133e087
tkrhinton <thomas.hinton@students.plymouth.ac.uk>
2020-01-07 17:19:46 +0000

    Creating api for the items table that gets values from the items table into json, Creating api for the orders table that gets values from the orders table into json,

---
+++ ISAD251/src/api/orders/index.php
@@ -0,0 +1,39 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../../../src/model/DbContext.php';
+include_once '../../../src/model/orders.php';
+
+$db = new DbContext();
+
+$response = $db->getAll('orderss');
+
+if($response)
+{
+    $code = 200;
+    echo returnJSON($response, $code);
+}
+else{
+
+    // set response code - 404 Not found
+    http_response_code(404);
+
+    // tell the user no products found
+    echo json_encode(
+        array("message" => "No products found.")
+    );
+}
+
+function returnJSON($response, $code)
+{
+    header_remove();
+    http_response_code($code);
+    header('Content-Type: application/json');
+    header('Status: '.$code);
+    return json_encode(array('status' => $code, 'message' => $response));
+}
+
+
+
